c182a9a862af0396bebe429f997c6afb2177bb80
AnaLuizaBarros <analuizagbarros@gmail.com>
2021-05-15 19:45:01 -0300

    Funções

---
+++ Dados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Dados
+    {
+        double valor;
+
+        public Dados() { }
+        public Dados(double Valor) {
+            valor = Valor;
+        }
+
+        public double Valor {get;set;}
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Usuario
+    {
+        string nome;
+        double valor;
+
+        public Usuario() { }
+        public Usuario(string Nome, double Valor) {
+            nome = Nome;
+            valor = Valor;
+        }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+    }
+}
